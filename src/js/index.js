0299a72a606c6680754920b8710b6f75b6289a51
Pedrael <45235838+Pedrael@users.noreply.github.com>
2020-05-21 14:10:48 +0300

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+import jQuery from 'jquery';
+import Header from './header.js';
+import Carousel from './carousel.js';
+//import MyTimeIsNow from './date.js';
+//import Mail from './mail.js';
+import Ripple from './ripple.js';
+
+Header();
+Carousel();
+//Mail();
+//MyTimeIsNow("date-input");
+Ripple();
